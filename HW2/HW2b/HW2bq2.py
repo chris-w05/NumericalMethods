2a8896308c0446f6565433225b1985653214d73b
Chris W <offthewalls.oodie@gmail.com>
2024-09-18 15:13:00 -0600

    Added images for HW and converted PDFs

--- HW2/HW2b/HW2bq2.py
+++ HW2/HW2b/HW2bq2.py
@@ -11,12 +11,14 @@ dr = .0015875       #meters -> 1/16 in
 dh = .0015875       #meters -> 1/16 in
 dP0 = 16.93194      #pascal -> .005 inHg
 dP1 = 6894.76       #pascal -> 1psi
-    
+
+#Error in Volume (partial derivatives of r and h)
 def errorV(r, h, dr, dh):
     #returns partial derivates in terms of r and h
     return [np.abs(math.pi*2*r*h*dr) , 
             np.abs(math.pi * r**2 * dh) ]
     
+#exergy formula
 def exergy(P0, P1, r, h):
     V = np.pi * r**2 * h
     return P1 * V * (math.log(P1/P0) + (P0/P1) - 1)
@@ -34,19 +36,20 @@ def error(P0, P1, r, h, dP0, dP1, dr, dh):
         dh (meters): Height uncertainty
     """
     V = math.pi * r**2 * h
-    #partial derivatives by
+    #partial derivatives multiplied by errors
     gradP0 = np.abs((V - (P1*V)/P0) * dP0)
     gradP1 = np.abs((V * math.log(P1/P0)) * dP1)
     dVr , dVh = errorV(r,h,dr, dh)
     gradr = np.abs((P1 * math.log(P1/P0) + P0 - P1) * (dVr))
     gradh = np.abs((P1 * math.log(P1/P0) + P0 - P1) * (dVh))
+    #printing errors
     print(f'Error from p0: {gradP0:.5} Joules \nError from P1: {gradP1:.5} Joules')
     print(f'Error from r: {gradr:.5} Joules')
     print(f'Error from h: {gradh:.5} Joules')
     print(f'Volume Errors:\n\tError from r: {dVr:.5} m^3\n\tError from h: {dVh:.5} m^3')
     return gradP0 + gradP1 + gradh + gradr
 
-
+#outputting results
 error = error(P0, P1, r, h, dP0, dP1, dr, dh)
 print(f'Total error: {error:.5} Joules' )
 print(f'Exergy = {exergy(P0, P1, r, h):.5} ± {error:.5} Joules') 
