cf5876967e2c87bbb96fe43c8f5610a541e0fc9d
Chris W <offthewalls.oodie@gmail.com>
2024-11-04 14:07:32 -0700

    Comments!

--- HW6/HW6p1.py
+++ HW6/HW6p1.py
@@ -7,9 +7,9 @@ def F(x):
 
 plt.figure()
 for i in [5, 2.5, 1.25, .625, .0375]:
-    params = [7/i**2 + 1/i ,
-              -14/i**2 - 1,
-              7/i**2 - 1/i]
+    params = [7/i**2 + 1/i ,    #alpha
+              -14/i**2 - 1,     #beta
+              7/i**2 - 1/i]     #gamma
     results, independent = Linsolve.BVPsolve(0, 20, 5, 8, i, params, F, solveMethod='Gauss')
     plt.plot(independent, results, label=f'h = {i}')
 plt.title('BVP solution')
